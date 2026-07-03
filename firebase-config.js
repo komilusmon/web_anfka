1b42e5c0f371c4fdb58ba64ecf0915cc99535c43
Komil <usmonqulovkomil80@gmail.com>
2026-07-03 10:19:24 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Firebase konfiguratsiyasi
+const firebaseConfig = {
+  apiKey: "AIzaSyD7ckJhmBkkY6p5WaMscrrJeaLDoysZ5Uo",
+  authDomain: "anfka-edcd9.firebaseapp.com",
+  projectId: "anfka-edcd9",
+  storageBucket: "anfka-edcd9.firebasestorage.app",
+  messagingSenderId: "264821445378",
+  appId: "1:264821445378:web:7594d3434e52fcd5a3281b"
+};
+
+// Firebase-ni ishga tushirish
+firebase.initializeApp(firebaseConfig);
+
+// Xizmatlarni olish
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+console.log('✅ Firebase muvaffaqiyatli ulandi');
